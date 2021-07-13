2e349542395bffec356bef6e0f92bfd968fb8017
Gaffal <gaffal.gamer.sjc@gmail.com>
2021-07-13 20:52:10 -0300

    Add plugin actions and quick improvements

--- src/ServerStorage/Main.server.lua
+++ src/ServerStorage/Main.server.lua
@@ -13,68 +13,78 @@ local ChangeHistoryService = game:GetService("ChangeHistoryService")
 
 local Toolbar = plugin:CreateToolbar("FolderTool")
 
-local Button_GroupIntoFolder = Toolbar:CreateButton(
-	"Group selected items into a folder",
-	"This button groups all items that are currently selected into a folder.",
-	"rbxassetid://7081112821"
-)
+local Images = {
+	Fold = 'rbxassetid://7081112821',
+	Unfold = 'rbxassetid://7081124672',
+}
 
-local Button_UngroupFromFolder = Toolbar:CreateButton(
-	"Ungroup items in selected folder",
-	"This button ungroups all items that are in the selected folder.",
-	"rbxassetid://7081124672"
-)
+local Buttons = {
+	Fold = Toolbar:CreateButton(
+		"Group selected items into a folder",
+		"This button groups all items that are currently selected into a folder.",
+		Images.Fold
+	),
 
+	Unfold = Toolbar:CreateButton(
+		"Ungroup items in selected folder",
+		"This button ungroups all items that are in the selected folder.",
+		Images.Unfold
+	)
+}
 
-Button_GroupIntoFolder.Click:Connect(function()
+local function fold_selection()
 	local selectedObjects = SelectionService:Get()
 
 	if #selectedObjects == 0 then
 		warn("You must have at least one Instance to group.")
 		return
 	end
-	
+
 	local sucess = pcall(function()
 		--\\ Some instances can't be moved. We check that using a pcall.
-		
+
 		for _, child in ipairs(selectedObjects) do
 			child.Parent = child.Parent
 		end
 	end)
-	
+
 	if not sucess then
 		warn("One item is not movable. Please un-select any services / any special Instances.")
 		return;
 	end
-	
+
 	local folder = Instance.new("Folder", selectedObjects[1].Parent)
 	for _, child in ipairs(selectedObjects) do
 		child.Parent = folder
 	end
-	
-	ChangeHistoryService:SetWaypoint("FolderTool: Grouped items into a folder")
-end)
 
-Button_UngroupFromFolder.Click:Connect(function()
+	SelectionService:Set({ folder }) -- Selecting the folder at the end
+	ChangeHistoryService:SetWaypoint("FolderTool: Grouped items into a folder")
+end
+
+local function unfold_selection()
 	local selectedObjects = SelectionService:Get()
 
 	if #selectedObjects == 0 then
 		warn("You need to select a folder first!")
 		return;
 	end
-	
+
 	for _, child in ipairs(selectedObjects) do
 		if not child:IsA("Folder") then
 			warn("You can only un-group folders.")
 			return
 		end
-		
+
 		--\\ Validation, just making sure every children is a folder.
 	end
-	
+
+	local ToBeSelected = {}
+
 	for _, folder in ipairs(selectedObjects) do
 		for _, child in ipairs(folder:GetChildren()) do
 			child.Parent = folder.Parent
+			table.insert(ToBeSelected, child)
 		end
 
 		folder.Parent = nil
@@ -82,6 +92,15 @@ Button_UngroupFromFolder.Click:Connect(function()
 		--   Because then, you can't click CTRL + Z to un-do it, as the folder is now locked, sadly.
 		--   However, this shouldn't have that big of a performance implication.
 	end
-	
+
+	SelectionService:Set(ToBeSelected) -- Selecting the children after ungrouping
 	ChangeHistoryService:SetWaypoint("FolderTool: Un-grouped items from a folder")
-end)
+end
+
+-- Buttons
+Buttons.Fold.Click:Connect(fold_selection)
+Buttons.Unfold.Click:Connect(unfold_selection)
+
+-- Actions
+plugin:CreatePluginAction('FoldObjects', 'Group selected items into a folder', 'Groups all items that are currently selected into a folder.').Triggered:Connect(fold_selection)
+plugin:CreatePluginAction('UnfoldObjects', 'Ungroup items in selected folder', 'This button ungroups all items that are in the selected folder.').Triggered:Connect(unfold_selection)
